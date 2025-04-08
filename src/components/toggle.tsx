8e8e3e18b3d06d87cd6458ee87c37f3de606edaf
Nichakorn <nichakon46@gmail.com>
2025-04-08 14:28:21 +0700

    add toggle

---
+++ src/components/toggle.tsx
@@ -0,0 +1,28 @@
+"use client";
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+export default function AndorToggleButton() {
+  const [alignment, setAlignment] = React.useState('/');
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string,
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+      aria-label="/"
+    >
+      <ToggleButton value="1">AND</ToggleButton>
+      <ToggleButton value="2">OR</ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
