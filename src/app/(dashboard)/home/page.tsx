7a0386815100fb4b8d3c07a6db72333085341c39
WislyHubHub <rachapruek12345@gmail.com>
2025-04-09 10:44:25 +0700

    update

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,8 +1,12 @@
 import DataTableComponent from '@/components/DataTable'
-import { CardContent } from '@mui/material'
+import { CardContent, Grid2 } from '@mui/material'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import * as React from 'react'
 
 export default function Page() {
   return (
-   <h1>Hello world</h1>
+    <h1>Home</h1>
   )
 }
